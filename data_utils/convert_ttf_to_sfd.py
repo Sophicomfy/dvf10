97a030b0297fbf1f84df3dc2855d631b71d531c7
Filip <flp@Filips-MacBook-Pro.local>
2024-05-31 08:14:33 +0200

    update

--- data_utils/convert_ttf_to_sfd.py
+++ data_utils/convert_ttf_to_sfd.py
@@ -2,28 +2,44 @@ import fontforge  # noqa
 import os
 import multiprocessing as mp
 import argparse
+import time
+from datetime import datetime
+import threading
 
 # conda deactivate
 # apt install python3-fontforge
 
 def convert_mp(opts):
-    """Useing multiprocessing to convert all fonts to sfd files"""
+    """Using multiprocessing to convert all fonts to sfd files"""
     charset = open(f"../data/char_set/{opts.language}.txt", 'r').read()
     charset_lenw = len(str(len(charset)))
-    fonts_file_path = os.path.join(opts.ttf_path, opts.language) # opts.ttf_path,opts.language, 
+    fonts_file_path = os.path.join(opts.ttf_path, opts.language)
     sfd_path = os.path.join(opts.sfd_path, opts.language)
     for root, dirs, files in os.walk(os.path.join(fonts_file_path, opts.split)):
         ttf_fnames = files
     
     font_num = len(ttf_fnames)
-    process_num = mp.cpu_count() - 2
+    print(f"Total number of fonts to process: {font_num}")  # Print total number of fonts at the beginning
+    process_num = min(opts.workers, mp.cpu_count() - 1)  # Adjust the number of workers
     font_num_per_process = font_num // process_num + 1
 
+    processed_fonts = mp.Value('i', 0)  # Shared counter to track processed fonts
+    task_queue = mp.Queue()
+    assigned_fonts = mp.Manager().list()  # Track assigned fonts
+
+    # Populate the task queue
+    for i in range(font_num):
+        task_queue.put(i)
+
     def process(process_id, font_num_p_process):
-        for i in range(process_id * font_num_p_process, (process_id + 1) * font_num_p_process):
-            if i >= font_num:
+        while not task_queue.empty():
+            try:
+                i = task_queue.get(timeout=1)
+                print(f"[{datetime.now()}] Process {process_id} assigned font {i} - {ttf_fnames[i]}")
+                assigned_fonts.append(i)
+            except queue.Empty:
                 break
-            
+
             font_id = ttf_fnames[i].split('.')[0]
             split = opts.split
             font_name = ttf_fnames[i]
@@ -31,6 +47,7 @@ def convert_mp(opts):
             font_file_path = os.path.join(fonts_file_path, split, font_name)
             try:
                 cur_font = fontforge.open(font_file_path)
+                cur_font.encoding = "UnicodeFull"  # Set encoding to UnicodeFull
             except Exception as e:
                 print('Cannot open ', font_name)
                 print(e)
@@ -46,41 +63,94 @@ def convert_mp(opts):
                 if opts.language == 'chn':
                     char = 'uni' + char.encode("unicode_escape")[2:].decode("utf-8")
                 
-                cur_font.selection.select(char)
+                cur_font.selection.select(ord(char))
                 cur_font.copy()
 
                 new_font_for_char = fontforge.font()
-                char = 'A'
-                new_font_for_char.selection.select(char)
+                new_font_for_char.encoding = "UnicodeFull"  # Set encoding to UnicodeFull
+                new_font_for_char.selection.select('A')
                 new_font_for_char.paste()
-                new_font_for_char.fontname = "{}_".format(font_id) + font_name
+                new_font_for_char.fontname = "{}_{}".format(font_id, font_name)
 
                 new_font_for_char.save(os.path.join(target_dir, '{}_{num:0{width}}.sfd'.format(font_id, num=char_id, width=charset_lenw)))
 
                 char_description.write(str(ord(char)) + '\n')
-                char_description.write(str(new_font_for_char[char].width) + '\n')
-                char_description.write(str(new_font_for_char[char].vwidth) + '\n')
+                char_description.write(str(new_font_for_char['A'].width) + '\n')
+                char_description.write(str(new_font_for_char['A'].vwidth) + '\n')
                 char_description.write('{num:0{width}}'.format(num=char_id, width=charset_lenw) + '\n')
                 char_description.write('{}'.format(font_id))
 
                 char_description.close()
 
             cur_font.close()
+            with processed_fonts.get_lock():
+                processed_fonts.value += 1
+
+            print(f"[{datetime.now()}] Process {process_id} processed font {processed_fonts.value}/{font_num} - {font_name}")
+
+    def monitor_processes(processes, task_queue, interval=10):
+        while any(p.is_alive() for p in processes) or not task_queue.empty():
+            remaining_fonts = task_queue.qsize()
+            running_processes = []
+            stopped_processes = []
+            restarted_processes = []
+            print(f"[{datetime.now()}] Checking running processes... Remaining fonts in queue: {remaining_fonts}")
+            for i, p in enumerate(processes):
+                if p.is_alive():
+                    running_processes.append(i)
+                else:
+                    stopped_processes.append(i)
+                    if not task_queue.empty():
+                        processes[i] = mp.Process(target=process, args=(i, font_num_per_process))
+                        processes[i].start()
+                        restarted_processes.append(i)
+
+            print(f"[{datetime.now()}] Processes report - Running: {running_processes}, Restarted: {restarted_processes}")
+
+            time.sleep(interval)
+
+        # Check for unprocessed fonts
+        if processed_fonts.value < font_num:
+            unprocessed_fonts = set(range(font_num)) - set(assigned_fonts)
+            if unprocessed_fonts:
+                print(f"[{datetime.now()}] Unprocessed fonts found: {unprocessed_fonts}")
+                # Re-populate the task queue with unprocessed fonts
+                for i in unprocessed_fonts:
+                    task_queue.put(i)
+                print(f"[{datetime.now()}] Re-populated task queue with unprocessed fonts.")
+                # Restart processes to handle unprocessed fonts
+                for i, p in enumerate(processes):
+                    if not p.is_alive():
+                        processes[i] = mp.Process(target=process, args=(i, font_num_per_process))
+                        processes[i].start()
+                        print(f"[{datetime.now()}] Restarted Process {i} to handle unprocessed fonts.")
 
     processes = [mp.Process(target=process, args=(pid, font_num_per_process)) for pid in range(process_num)]
 
+    start_time = time.time()
     for p in processes:
         p.start()
+    
+    monitor_thread = threading.Thread(target=monitor_processes, args=(processes, task_queue))
+    monitor_thread.start()
+
     for p in processes:
         p.join()
+    monitor_thread.join()
+    
+    end_time = time.time()
+
+    print(f"Total number of fonts processed: {processed_fonts.value}")  # Print total number of fonts processed
+    print(f"Total processing time: {end_time - start_time:.2f} seconds")
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Convert ttf fonts to sfd fonts")
+    parser = argparse.ArgumentParser(description="Convert ttf fonts to sfd files")
     parser.add_argument("--language", type=str, default='eng', choices=['eng', 'chn'])
     parser.add_argument("--ttf_path", type=str, default='../data/font_ttfs')
     parser.add_argument('--sfd_path', type=str, default='../data/font_sfds')
     parser.add_argument('--split', type=str, default='train')
+    parser.add_argument('--workers', type=int, default=mp.cpu_count() - 2, help='Number of worker processes to run')
     opts = parser.parse_args()
     convert_mp(opts)
 
--- data_utils/write_data_to_dirs_create_db.py
+++ data_utils/write_data_to_dirs_create_db.py
@@ -3,6 +3,7 @@ import numpy as np
 import multiprocessing as mp
 import svg_utils
 from write_data_to_dirs_utils import exist_empty_imgs
+from write_data_to_dirs_log import log_progress
 
 def create_db(opts, output_path, log_path):
     charset = open(f"../data/char_set/{opts.language}.txt", 'r').read()
@@ -16,10 +17,9 @@ def create_db(opts, output_path, log_path):
     fonts_per_process = num_fonts // num_processes + 1
     num_chars = len(charset)
     num_chars_w = len(str(num_chars))
-
+    processed_fonts = mp.Value('i', 0)  # Shared variable to count processed fonts
 
     def process(process_id):
-
         cur_process_log_file = open(os.path.join(log_path, f'log_{opts.split}_{process_id}.txt'), 'w')
         for i in range(process_id * fonts_per_process, (process_id + 1) * fonts_per_process):
             if i >= num_fonts:
@@ -30,10 +30,9 @@ def create_db(opts, output_path, log_path):
 
             if not os.path.exists(os.path.join(cur_font_sfd_dir, 'imgs_' + str(opts.img_size) + '.npy')):
                 continue
-            
-            # a whole font as an entry
+
             for char_id in range(num_chars):
-            #    print(char_id)
+                print(char_id)
                 if not os.path.exists(os.path.join(cur_font_sfd_dir, '{}_{num:0{width}}.sfd'.format(font_id, num=char_id, width=num_chars_w))):
                     break
 
@@ -48,21 +47,22 @@ def create_db(opts, output_path, log_path):
                 char_idx = char_desp[3].strip()
                 font_idx = char_desp[4].strip()
 
-                cur_glyph = {}
-                cur_glyph['uni'] = uni
-                cur_glyph['width'] = width
-                cur_glyph['vwidth'] = vwidth
-                cur_glyph['sfd'] = sfd
-                cur_glyph['id'] = char_idx
-                cur_glyph['binary_fp'] = font_idx
+                cur_glyph = {
+                    'uni': uni,
+                    'width': width,
+                    'vwidth': vwidth,
+                    'sfd': sfd,
+                    'id': char_idx,
+                    'binary_fp': font_idx
+                }
 
                 if not svg_utils.is_valid_glyph(cur_glyph):
                     msg = f"font {font_idx}, char {char_idx} is not a valid glyph\n"
                     cur_process_log_file.write(msg)
                     char_desp_f.close()
                     sfd_f.close()
-                    # use the font whose all glyphs are valid
                     break
+
                 pathunibfp = svg_utils.convert_to_path(cur_glyph)
 
                 if not svg_utils.is_valid_path(pathunibfp):
@@ -73,20 +73,16 @@ def create_db(opts, output_path, log_path):
                     break
 
                 example = svg_utils.create_example(pathunibfp)
-
                 cur_font_glyphs.append(example)
                 char_desp_f.close()
                 sfd_f.close()
-            
-            if len(cur_font_glyphs) == num_chars:
-                # use the font whose all glyphs are valid
-                # merge the whole font
 
+            if len(cur_font_glyphs) == num_chars:
                 rendered = np.load(os.path.join(cur_font_sfd_dir, 'imgs_' + str(opts.img_size) + '.npy'))
 
                 if (rendered[0] == rendered[1]).all() == True:
                     continue
- 
+
                 sequence = []
                 seq_len = []
                 binaryfp = []
@@ -97,22 +93,23 @@ def create_db(opts, output_path, log_path):
                     seq_len.append(example['seq_len'])
                     char_class.append(example['class'])
                     binaryfp = example['binary_fp']
-                if not os.path.exists(os.path.join(output_path, '{num:0{width}}'.format(num=i, width=num_fonts_w))):
-                    os.mkdir(os.path.join(output_path, '{num:0{width}}'.format(num=i, width=num_fonts_w)))
+                font_dir = os.path.join(output_path, '{num:0{width}}'.format(num=i, width=num_fonts_w))
+                if not os.path.exists(font_dir):
+                    os.mkdir(font_dir)
 
-                np.save(os.path.join(output_path, '{num:0{width}}'.format(num=i, width=num_fonts_w), 'sequence.npy'), np.array(sequence))
-                np.save(os.path.join(output_path, '{num:0{width}}'.format(num=i, width=num_fonts_w), 'seq_len.npy'), np.array(seq_len))
-                np.save(os.path.join(output_path, '{num:0{width}}'.format(num=i, width=num_fonts_w), 'class.npy'), np.array(char_class))
-                np.save(os.path.join(output_path, '{num:0{width}}'.format(num=i, width=num_fonts_w), 'font_id.npy'), np.array(binaryfp))
-                np.save(os.path.join(output_path, '{num:0{width}}'.format(num=i, width=num_fonts_w), 'rendered_' + str(opts.img_size) + '.npy'), rendered)
+                np.save(os.path.join(font_dir, 'sequence.npy'), np.array(sequence))
+                np.save(os.path.join(font_dir, 'seq_len.npy'), np.array(seq_len))
+                np.save(os.path.join(font_dir, 'class.npy'), np.array(char_class))
+                np.save(os.path.join(font_dir, 'font_id.npy'), np.array(binaryfp))
+                np.save(os.path.join(font_dir, 'rendered_' + str(opts.img_size) + '.npy'), rendered)
 
                 with processed_fonts.get_lock():
                     processed_fonts.value += 1
+
                 log_progress(process_id, processed_fonts, num_fonts, font_id, font_dir)
-    
+
     while processed_fonts.value < num_fonts:
         processes = [mp.Process(target=process, args=[pid]) for pid in range(num_processes)]
-
         for p in processes:
             p.start()
         for p in processes:
