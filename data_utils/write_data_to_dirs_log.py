d55d1f5a7ca536058049e2be182df8a14dc6067c
Filip <flp@Filips-MacBook-Pro.local>
2024-05-31 07:27:46 +0200

    remove unnecessary comments

--- data_utils/write_data_to_dirs_log.py
+++ data_utils/write_data_to_dirs_log.py
@@ -1,16 +1,6 @@
 from datetime import datetime
 
 def log_progress(process_id, processed_fonts, font_num, fontname, output_path):
-    """
-    Logs detailed progress of the font processing.
-
-    Args:
-        process_id (int): The ID of the process.
-        processed_fonts (Value): Shared variable to count processed fonts.
-        font_num (int): Total number of fonts.
-        fontname (str): Name of the processed font.
-        output_path (str): The path where the .npy files are saved.
-    """
     sequence_path = f"{output_path}/sequence.npy"
     seq_len_path = f"{output_path}/seq_len.npy"
     class_path = f"{output_path}/class.npy"
