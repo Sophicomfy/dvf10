2b20d435fa71fd06b3b7b95714a32cf1f95b7347
Filip <flp@Filips-MacBook-Pro.local>
2024-06-06 16:00:58 +0200

    Update code to read characters separated by `Newline-Delimited` Print characters as unicode

--- data_utils_ext/convert_ttf_to_sfd.py
+++ data_utils_ext/convert_ttf_to_sfd.py
@@ -5,7 +5,8 @@ import data_preprocess_options
 
 def convert_mp(opts):
     """Using multiprocessing to convert all fonts to sfd files"""
-    charset = open(opts.charset_path, 'r').read()
+    with open(opts.charset_path, 'r') as f:
+        charset = [line.strip() for line in f if line.strip()]
     charset_lenw = len(str(len(charset)))
     fonts_file_path = opts.ttf_path
     sfd_path = opts.sfd_path
@@ -40,28 +41,33 @@ def convert_mp(opts):
                 char_description = open(os.path.join(target_dir, '{}_{num:0{width}}.txt'.format(font_id, num=char_id, width=charset_lenw)), 'w')
 
                 # Default Unicode handling
-                char = 'uni' + char.encode("unicode_escape")[2:].decode("utf-8")
+                if not char.startswith('uni'):
+                    char = 'uni' + char.encode("unicode_escape")[2:].decode("utf-8")
                 
-                cur_font.selection.select(char)
-                cur_font.copy()
+                try:
+                    cur_font.selection.select(char)
+                    cur_font.copy()
 
-                new_font_for_char = fontforge.font()
-                new_font_for_char.selection.select('A')
-                new_font_for_char.paste()
-                new_font_for_char.fontname = "{}_".format(font_id) + font_name
+                    new_font_for_char = fontforge.font()
+                    new_font_for_char.selection.select('A')
+                    new_font_for_char.paste()
+                    new_font_for_char.fontname = "{}_".format(font_id) + font_name
 
-                if opts.margin:
-                    new_font_for_char['A'].left_side_bearing = opts.margin
-                    new_font_for_char['A'].right_side_bearing = opts.margin
+                    if opts.margin:
+                        new_font_for_char['A'].left_side_bearing = opts.margin
+                        new_font_for_char['A'].right_side_bearing = opts.margin
 
-                new_font_for_char.save(os.path.join(target_dir, '{}_{num:0{width}}.sfd'.format(font_id, num=char_id, width=charset_lenw)))
+                    new_font_for_char.save(os.path.join(target_dir, '{}_{num:0{width}}.sfd'.format(font_id, num=char_id, width=charset_lenw)))
 
-                char_description.write(str(ord(char)) + '\n')
-                char_description.write(str(new_font_for_char['A'].width) + '\n')
-                char_description.write(str(new_font_for_char['A'].vwidth) + '\n')
-                char_description.write('{num:0{width}}'.format(num=char_id, width=charset_lenw) + '\n')
-                char_description.write('{}'.format(font_id))
+                    char_description.write(str(ord(char)) + '\n')
+                    char_description.write(str(new_font_for_char['A'].width) + '\n')
+                    char_description.write(str(new_font_for_char['A'].vwidth) + '\n')
+                    char_description.write('{num:0{width}}'.format(num=char_id, width=charset_lenw) + '\n')
+                    char_description.write('{}'.format(font_id))
 
+                except Exception as e:
+                    print(f'Error processing character {char}: {e}')
+                
                 char_description.close()
 
             cur_font.close()
--- data_utils_ext/print_charset_unicode.py
+++ data_utils_ext/print_charset_unicode.py
@@ -6,16 +6,12 @@ font = fontforge.open("/datasets/lttr_ext/00000.otf")
 # Get the list of glyphs in the font
 glyphs = font.glyphs()
 
-# Print the character set
+# Print the character set in Newline-Delimited Unicode format
 print("Character Set of the Font:")
 for glyph in glyphs:
     if glyph.unicode != -1:  # Only consider glyphs with valid Unicode values
         unicode_hex = f"{glyph.unicode:04X}"
-        if glyph.unicode < 128:
-            print(chr(glyph.unicode), end=' ')
-        else:
-            print(f"uni{unicode_hex}", end=' ')
-print()  # For a new line at the end
+        print(f"uni{unicode_hex}")
 
 # Close the font file
 font.close()
