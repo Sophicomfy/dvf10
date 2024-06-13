eabb2d7bf2f72283bf610c8c631ed706db564a00
Filip <flp@Filips-MacBook-Pro.local>
2024-06-13 14:27:48 +0200

    arguments transfer to central options script

--- data_utils_ext/data_preprocess_options.py
+++ data_utils_ext/data_preprocess_options.py
@@ -6,10 +6,12 @@ def get_data_preprocess_options():
     parser.add_argument("--ttf_path", type=str, required=True, help='Path to TTF font files directory')
     parser.add_argument('--sfd_path', type=str, required=True, help='Path to save SFD font files')
     parser.add_argument('--charset_path', type=str, required=True, help='Path to charset.txt file')
+    parser.add_argument('--output_path', type=str, required=True, help="Path to write the database to")  # Added argument
+    parser.add_argument('--img_size', type=int, default=64, help='Image size for glyphs')
     parser.add_argument('--workers', type=int, default=mp.cpu_count() - 1, help='Number of worker processes to use')
     parser.add_argument('--margin', type=int, default=0, help='Margin for character bounding box')
-    parser.add_argument('--img_size', type=int, default=64, help='Image size for glyphs')  # Added argument
-    parser.add_argument('--debug', type=bool, default=False, help='Enable debug mode')  # Added argument
+    parser.add_argument('--phase', type=int, default=0, choices=[0, 1, 2], help='0 all, 1 create db, 2 cal stddev')  # Added argument
+    parser.add_argument('--debug', type=bool, default=False, help='Enable debug mode')
     return parser
 
 if __name__ == "__main__":
--- data_utils_ext/write_data_to_dirs.py
+++ data_utils_ext/write_data_to_dirs.py
@@ -1,18 +1,12 @@
-import argparse
 import os
 from write_data_to_dirs_create_db import create_db
 from write_data_to_dirs_cal_mean_stddev import cal_mean_stddev
+from data_preprocess_options import get_data_preprocess_options
 
 def main():
-    parser = argparse.ArgumentParser(description="LMDB creation")
-    parser.add_argument("--ttf_path", type=str, default='../data/font_ttfs')
-    parser.add_argument('--sfd_path', type=str, default='../data/font_sfds')
-    parser.add_argument("--output_path", type=str, default='../data/vecfont_dataset_/', help="Path to write the database to")
-    parser.add_argument('--img_size', type=int, default=64, help="the height and width of glyph images")
-    parser.add_argument("--charset_path", type=str, required=True, help="Path to charset.txt file")
-    parser.add_argument("--phase", type=int, default=0, choices=[0, 1, 2], help="0 all, 1 create db, 2 cal stddev")
-
+    parser = get_data_preprocess_options()
     opts = parser.parse_args()
+
     assert os.path.exists(opts.sfd_path), "specified sfd glyphs path does not exist"
     assert os.path.exists(opts.charset_path), "specified charset path does not exist"
 
