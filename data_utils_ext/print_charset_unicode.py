48648bf5452235d6283a7f3c4b1eccdec0c8cbe0
Filip <flp@Filips-MacBook-Pro.local>
2024-06-13 09:14:16 +0200

    add variations to print charset

--- data_utils_ext/print_charset_unicode.py
+++ data_utils_ext/print_charset_unicode.py
@@ -1,17 +1,53 @@
-# run by: `fontforge -script /path/to/print_charset.pe`
+import fontforge
+import sys
+import getopt
 
-# Open the font file
-font = fontforge.open("/datasets/lttr_ext/00000.otf")
+def usage():
+    print("Usage: fontforge -script print_charset.pe --font /path/to/font/file.ttf --glyph uni|id|charname|char")
+    sys.exit(2)
 
-# Get the list of glyphs in the font
-glyphs = font.glyphs()
+def main(argv):
+    font_path = None
+    glyph_types = []
 
-# Print the character set in Newline-Delimited Unicode format
-print("Character Set of the Font:")
-for glyph in glyphs:
-    if glyph.unicode != -1:  # Only consider glyphs with valid Unicode values
-        unicode_hex = f"{glyph.unicode:04X}"
-        print(f"uni{unicode_hex}")
+    try:
+        opts, args = getopt.getopt(argv, "", ["font=", "glyph="])
+    except getopt.GetoptError:
+        usage()
 
-# Close the font file
-font.close()
+    for opt, arg in opts:
+        if opt == "--font":
+            font_path = arg
+        elif opt == "--glyph":
+            glyph_types = arg.split()
+    
+    if not font_path or not glyph_types:
+        usage()
+
+    # Open the font file
+    font = fontforge.open(font_path)
+
+    # Get the list of glyphs in the font
+    glyphs = font.glyphs()
+
+    # Print the character set
+    print("Character Set of the Font:")
+    for glyph in glyphs:
+        if glyph.unicode != -1:  # Only consider glyphs with valid Unicode values
+            output = []
+            if "uni" in glyph_types:
+                output.append(f"U{glyph.unicode:04X}")
+            if "id" in glyph_types:
+                output.append(str(glyph.unicode))
+            if "charname" in glyph_types:
+                output.append(glyph.glyphname)
+            if "char" in glyph_types:
+                output.append(chr(glyph.unicode))
+            print(", ".join(output))
+    print()  # For a new line at the end
+
+    # Close the font file
+    font.close()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
